dfd7771fd30643fffb26364d1d0b82b5110b987f
Daniel Grumberg <dany.grumberg@gmail.com>
2017-10-18 16:10:31 +0100

    Fix the parser's inner loop structure

--- parser.c
+++ parser.c
@@ -39,6 +39,31 @@ static formula_t *parse_problem_line(FILE *fp)
     return alloc_formula(num_clauses, num_variables);
 }
 
+static int parse_clause(formula_t *formula, FILE *fp)
+{
+    int *variables = calloc(formula->num_variables, sizeof (int));
+
+    //This loop is completely fucked, works by chance (the break when we reach 0)
+    int var;
+    fscanf(fp, "%d ", &var);
+    for(; var != 0; fscanf(fp, "%d ", &var))
+    {
+        if (abs(var) > formula->num_variables)
+        {
+            fprintf(stderr, "Unknown variable: %d at clause %u\n", var, c);
+            // Report the error to the caller
+            return 1;
+        }
+
+        // Convert var to 0-based variable index, ternary operator handles the negative case
+        var = var < 0 ? var++ : var--;
+        variables[abs(var)] = var;
+    }
+
+    add_clause(formula, variables);
+    return 0;
+}
+
 formula_t *parse_dimacs_file(char *path)
 {
     // Need to decalre this here in case the fopen call fails so we have smth to return
@@ -57,29 +82,9 @@ formula_t *parse_dimacs_file(char *path)
     for (unsigned c = 0; c < formula->num_clauses; ++c)
     {
         skip_comments(fp);
-        int *variables = calloc(formula->num_variables, sizeof (int));
 
-        //This loop is completely fucked, works by chance (the break when we reach 0)
-        for (int v = 0; v < formula->num_variables; ++v)
-        {
-            int var;
-            fscanf(fp, "%d ", &var);
-
-            if (var == 0)
-                break;
-
-            if (abs(var) > formula->num_variables)
-            {
-                fprintf(stderr, "Unknown variable: %d at clause %u\n", var, c);
-                goto cleanup;
-            }
-
-            // Convert var to 0-based variable index, ternary operator handles the negative case
-            var = var < 0 ? var++ : var--;
-            variables[abs(var)] = var;
-        }
-
-        add_clause(formula, variables);
+        if(parse_clause(formula, fp))
+            goto cleanup;
     }
 
 cleanup:
