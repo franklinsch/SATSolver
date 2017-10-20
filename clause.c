c455927484e915fccb657eb4be84452054b19c88
Elias Benussi <elias.benussi14@imperial.ac.uk>
2017-10-20 11:15:35 +0100

    Simplify expression with terms that cancel out

--- clause.c
+++ clause.c
@@ -54,7 +54,7 @@ void clause_delete_var(clause_t *clause, size_t index)
     // Shift all the remaining elements to the left
     int *dst = clause->variables;
     int *src = clause->variables + 1;
-    size_t amount = (clause->variables + clause->size) - (clause->variables + index + 1);
+    size_t amount = clause->size - index -1;
     memmove(dst, src, amount);
 
     clause->size--;
