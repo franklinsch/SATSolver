18ac9ca4348f96c1df12e0f406ff15bcc0f92407
Franklin Schrans <f.schrans@me.com>
2017-10-21 10:56:06 +0100

    Add stubs for DPLL interface

---
+++ dpll.c
@@ -0,0 +1,17 @@
+#include "dpll.h"
+
+#include <stdbool.h>
+
+static formula_t *bcp(formula_t *formula);
+
+bool dpll(formula_t *formula) 
+{
+}
+
+/*
+ * Applies Boolean Constant Propagation to `formula`, returning the resulting formula.
+ */
+static formula_t *bcp(formula_t *formula)
+{
+  return NULL;
+}
---
+++ dpll.h
@@ -0,0 +1,9 @@
+#ifndef DPLL_H
+#define DPLL_H
+
+#include <stdbool.h>
+#include <formula.h>
+
+bool dpll(formula_t *formula);
+
+#endif //!DPLL_H
