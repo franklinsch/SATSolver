4d9771117b7d6fca0ff0a0c1ce05417da94a9eae
Franklin Schrans <f.schrans@me.com>
2017-10-21 10:40:41 +0100

    Edit comments for clause interface

--- clause.h
+++ clause.h
@@ -18,13 +18,16 @@ int clause_init(clause_t *clause);
 /*
  * Reserve in advance if size is known
  */
-
 void clause_reserve(clause_t *clause, size_t capacity);
 
 /*
- * Adding and removing variables from the clause
+ * Adds a variable to the clause. Returns the new size of the clause.
  */
 size_t clause_add_var(clause_t *clause, int var);
+
+/*
+ * Removes a variable from a clause.
+ */
 void clause_delete_var(clause_t *clause, size_t index);
 
 /*
@@ -34,7 +37,7 @@ int clause_get_var(clause_t *clause, size_t index);
 int clause_toggle_var(clause_t *clause, size_t index);
 
 /*
- * Free all ressources associated to a clause
+ * Free all resources associated to a clause
  */
 void clause_free(clause_t *clause);
 
