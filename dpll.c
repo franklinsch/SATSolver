d2ca91965083f98fe4b3ccedae1c62a1943e5b86
Franklin Schrans <f.schrans@me.com>
2017-10-21 11:50:15 +0100

    Start implementing trivial assignments

--- dpll.c
+++ dpll.c
@@ -1,11 +1,55 @@
 #include "dpll.h"
 
+#include <stdlib.h>
+#include <assert.h>
 #include <stdbool.h>
 
+#include <clause.h>
+#include <list.h>
+
+// -- Helper data structures.
+
+typedef struct {
+  clause_t *clause;
+  
+  // The indices of the watched literals in the clause (2 maximum).
+  int watch_literals_indices[2];
+} bcp_clause_t;
+
+// Represents an assignment to be made to a literal.
+typedef struct {
+  int literal;
+  
+  // The target value of literal after the assignment.
+  bool assignment;
+} assignment_t;
+
+// -- Forward function declarations
+
 static formula_t *bcp(formula_t *formula);
+static list_t *get_trivial_assignments(formula_t *formula);
+
+// -- Implementations
 
 bool dpll(formula_t *formula) 
 {
+  assert(formula->num_variables > 0);
+  bool assignments[formula->num_variables];
+  // list_t assignment_queue = list_alloc();
+  
+  // Get all trivial assignments.
+
+  list_t *trivial_assignments = get_trivial_assignments(formula);
+  for (size_t i = 0; i < trivial_assignments->_size; ++i)
+  {
+    int assignment = list_get_at(trivial_assignments, i)->value;
+    assignments[abs(assignment)] = assignment;
+  }
+
+
+  // hashmap_t *watch_literal_map = hashmap_alloc();
+  
+  return false;
 }
 
 /*
@@ -14,4 +58,23 @@ bool dpll(formula_t *formula)
 static formula_t *bcp(formula_t *formula)
 {
   return NULL;
+}
+
+/*
+ * Returns the list of assignments implied by size one clauses.
+ */
+static list_t *get_trivial_assignments(formula_t *formula)
+{
+  list_t *assignments = list_new();
+
+  clause_t *end = formula->clauses + formula->num_clauses;
+  for (clause_t *c = formula->clauses; c < end; ++c) 
+  {
+    if (c->size == 1) 
+    {
+      list_append(assignments, c->variables[0]);
+    }
+  }
+
+  return assignments;
 }
