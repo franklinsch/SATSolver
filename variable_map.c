09955110bc102c34f45c3e691cb8c846d8ee4920
Daniel Grumberg <dany.grumberg@gmail.com>
2017-10-29 22:18:50 +0000

    Add map interface and all functions except from add and remove

--- variable_map.c
+++ variable_map.c
@@ -1,8 +1,94 @@
 #include "variable_map.h"
 
+#include <assert.h>
 #include <stdlib.h>
 
-void variable_map_init(variable_map_t *map, size_t num_variables)
-{
+#define DEFAULT_NUM_BUCKETS 16
+#define DEFAULT_LOAD_FACTOR 1.0f
+// A variable cannot be 0 so we use this value to signify the start of the bucket list info
+#define BUCKET_HEADER_KEY 0
 
+typedef struct variable_map_entry_t
+{
+    int variable;
+    void *value;
+    struct variable_map_entry_t *_next_entry;
+} variable_map_entry_t;
+
+static size_t _hash_variable(variable_map_t *map, const int variable)
+{
+    return variable < 0 ? variable : (abs(variable) + map->_num_variables);
 }
+
+static void _free_bucket(variable_map_entry_t *bucket, size_t num_entries)
+{
+    assert(bucket->variable == BUCKET_HEADER_KEY);
+
+    variable_map_entry_t *curr_entry = bucket->_next_entry;
+    bucket->_next_entry = NULL;
+
+    while (curr_entry != NULL)
+    {
+        variable_map_entry_t *next = curr_entry->_next_entry;
+        free(curr_entry);
+        curr_entry = next;
+    }
+}
+
+static variable_map_entry_t* _search_bucket(const int variable, const variable_map_entry_t *bucket)
+{
+    assert(bucket->variable == BUCKET_HEADER_KEY);
+
+    variable_map_entry_t *curr_entry = bucket->_next_entry;
+    bucket->_next_entry = NULL;
+
+    while (curr_entry != NULL)
+    {
+        if (curr_entry->variable == variable)
+        {
+            return curr_entry;
+        }
+
+        variable_map_entry_t *next = curr_entry->_next_entry;
+        curr_entry = next;
+    }
+}
+
+void variable_map_init(variable_map_t *map, const size_t num_buckets, const float load_factor, const int num_variables)
+{
+    map->_num_variables = num_variables;
+    map->_used_buckets = 0;
+    map->_num_buckets = num_buckets ? num_buckets : DEFAULT_NUM_BUCKETS;
+    map->_load_factor = load_factor == 0.0f ? load_factor : DEFAULT_LOAD_FACTOR;
+
+    map->_buckets = malloc(num_buckets * sizeof (variable_map_entry_t));
+
+    variable_map_entry_t *buckets_end = map->_buckets + map->num_buckets;
+    for (variable_map_entry_t *bucket = map->_buckets; bucket < buckets_end; bucket++)
+    {
+        bucket->variable = BUCKET_HEADER_KEY;
+        // Terminator for bucket chain
+        bucket->_next_entry = NULL;
+    }
+}
+
+void variable_map_free(variable_map_t *map)
+{
+    variable_map_entry_t *buckets_end = map->_buckets + map->num_buckets;
+    for (variable_map_entry_t *bucket = map->_buckets; bucket < buckets_end; bucket++)
+    {
+        assert(bucket->variable == BUCKET_HEADER_KEY);
+        _free_bucket(bucket);
+    }
+
+    free(map->_buckets);
+}
+
+void *variable_map_get(variable_map_t *map, const int variable)
+{
+    variable_map_entry_t *bucket = map->_buckets + (_hash_variable(map, variable) & (map->_num_buckets - 1));
+    variable_map_entry_t *entry = _search_bucket(variable, bucket);
+    return entry->value;
+}
+
+
--- variable_map.h
+++ variable_map.h
@@ -1,18 +1,34 @@
 #ifndef DPLL_H
 #define DPLL_H
 
-typedef strcut variable_map_t
+
+struct variable_map_entry_t;
+/**
+ Represents a hash map of variables to (void *). Duplicates are not allowed.
+*/
+typedef struct variable_map_t
 {
-    void **values;
-    size_t num_values;
-}
+    struct variable_map_entry_t *_buckets;
+    size_t _used_buckets;
+    // Needs to be a power of 2
+    size_t _num_buckets;
+    float _load_factor;
+    int _num_variables;
+} variable_map_t;
 
 /**
  Initialises a map of variables.
 
- @param variables The number of variables in the formula, which is half the number of possible keys
+ @param num_buckets The number of buckets to use initially. A value of 0 indicates the default number.
+ @param load_factor The maximum allowed load factor before a resize. A value of 0 indicates the default value.
+ @param num_variables The total number of indexable variables
 */
-void variable_map_init(variable_map_t *map, size_t num_tvariables);
+void variable_map_init(variable_map_t *map, const size_t num_buckets, const float load_factor, const int num_variables);
+
+/**
+ Deallocate the supplied map.
+*/
+void variable_map_free(variable_map_t * const map);
 
 /**
  Add a mapping from the pecified variable to the specified value.
@@ -20,20 +36,21 @@ void variable_map_init(variable_map_t *map, size_t num_tvariables);
  @param variable The variable to use as the key for this mapping.
  @param value A pointer to the value to be mapped to.
 */
-void variable_map_add(variable_map_t *map, int variable, void *value);
+void variable_map_add(variable_map_t *map, const int variable, const void *value);
 
 /**
  Get the value associated with the specified key.
 
  @param variable The variable to get the value for.
+ @return Returns a NULL pointer if the variable is not present in the map, otherwise a pointer to the contained value
 */
-void variable_map_get(variable_map_t *map, int variable);
+void *variable_map_get(variable_map_t *map, const int variable);
 
 /**
  Remove the mapping associated with the specified key.
 
- @param variable The variable to remove the mappingfor.
+ @param variable The variable to remove the mapping for.
 */
-void variable_map_remove(variable_map_t *map, int variable);
+void variable_map_remove(variable_map_t *map, const int variable);
 
 #endif // !DPLL_H
