3b24a6dfd04d6e3097de061736b72ddadd621bed
Daniel Grumberg <dany.grumberg@gmail.com>
2017-10-31 13:55:22 +0000

    Add unit tests for variable map

--- unit-test/variable_map.c
+++ unit-test/variable_map.c
@@ -1,8 +1,26 @@
+#include "check.h"
 #include "variable_map.h"
 
-int main(int argc, char *argv[])
+int main(void)
 {
     variable_map_t map;
-    variable_map_init(&map, 0, 0.0f, 4);
+    variable_map_init(&map, 4);
+
+    variable_map_add(&map, 1, (void *) 1);
+    variable_map_add(&map, 4,(void *)  4);
+    variable_map_add(&map, -1, (void *) -1);
+    variable_map_add(&map, -4, (void *) -4);
+
+    void *val = variable_map_get(&map, 1);
+    CHECK( (int) val == 1 );
+
+    val = variable_map_get(&map, 4);
+    CHECK( (int) val == 4 );
+
+    val = variable_map_get(&map, -1);
+    CHECK( (int) val == -1 );
+
+    val = variable_map_get(&map, -4);
+    CHECK( (int) val == -4 );
     return 0;
 }
